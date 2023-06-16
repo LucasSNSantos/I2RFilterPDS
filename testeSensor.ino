4597da4fa151eb9e15c4c1561e9e5d3e5d19f48c
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2023-06-15 21:56:57 -0300

    Update testeSensor.ino
    implementação básica de um passa baixas, com sinal filtrado e sinal coletado original

--- testeSensor.ino
+++ testeSensor.ino
@@ -6,13 +6,35 @@
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
+
+void deslocaVetor( float *vetorAddr, int tam, float valor){
+    for (int k = tam-1; k > 0; k--){
+        *(vetorAddr+k) = *(vetorAddr+k-1);
+    }
+    *vetorAddr = valor;
+}
+
+float medido = 0;
+float filtrado = 0;
+float filtroPassaBaixa (float x){
+    static float y_pass[2] = {0,0}, x_pass[2] = {0,0};
+    const float a = 0.65, b = 0.70;
+    float y = (a+b)* y_pass[0] - a * b * y_pass[1] + (1 - a - b + a * b) * x_pass[1];
+    deslocaVetor(y_pass, 2, y);
+    deslocaVetor(x_pass, 2, x);
+    return y;
+}
+
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(100);
-  Serial.println(sonar.ping_cm());
+  delay(50);
+  medido = sonar.ping_cm();
+  filtrado = filtroPassaBaixa(medido);
+  Serial.print(medido);
+  Serial.print(",");
+  Serial.println(filtrado);  
 }
