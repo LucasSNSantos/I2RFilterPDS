cb835fa4c18f3cc650adbfe147014e8ec50b5cf6
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2023-06-17 19:53:16 -0300

    Update testeSensor.ino
    Botão para iniciar medição, e tempo limite para realizar a mesma

--- testeSensor.ino
+++ testeSensor.ino
@@ -3,6 +3,8 @@
 #define TRIGGER_PIN 12
 #define ECHO_PIN 11
 #define MAX_DISTANCE 30
+#define BOTAO 7
+#define TEMPO_MAX 3000
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
@@ -16,6 +18,8 @@ void deslocaVetor( float *vetorAddr, int tam, float valor){
 
 float medido = 0;
 float filtrado = 0;
+float time = 0;
+bool btnPress = false;
 float filtroPassaBaixa (float x){
     static float y_pass[2] = {0,0}, x_pass[2] = {0,0};
     const float a = 0.65, b = 0.70;
@@ -28,13 +32,20 @@ float filtroPassaBaixa (float x){
 
 void setup() {
   Serial.begin(9600);
+  pinMode(BOTAO, INPUT_PULLUP);
 }
 
 void loop() {
-  delay(50);
-  medido = sonar.ping_cm();
-  filtrado = filtroPassaBaixa(medido);
-  Serial.print(medido);
-  Serial.print(",");
-  Serial.println(filtrado);  
+  if(digitalRead(BOTAO) == LOW) { 
+    btnPress = !btnPress;
+  }
+
+  if(btnPress && time < TEMPO_MAX)
+  {
+    delay(50);
+    medido = sonar.ping_cm();
+    filtrado = filtroPassaBaixa(medido);
+    Serial.println(medido);
+    time++;
+  }
 }
