4a7702a690fdd21c40fb9d3d02f2d8e302b95a5c
Lucas Souza <42611011+LucasSNSantos@users.noreply.github.com>
2023-06-20 17:13:55 -0300

    Update testeSensor.ino
    filtro correto

--- testeSensor.ino
+++ testeSensor.ino
@@ -5,7 +5,6 @@
 #define MAX_DISTANCE 30
 #define BOTAO 7
 #define TEMPO_MAX 3000
-
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 
@@ -23,25 +22,21 @@ bool btnPress = false;
 float filtroPassaBaixa (float x){
     static float y_pass[2] = {0,0}, x_pass[2] = {0,0};
     const float a = 0.65, b = 0.70;
-    const float a0 = 3.414, a1 = 0, a2 = 0.586;
+    const float a0 = 0.7616, a1 = -2.2228, a2 = 0.0156;
     const float b0 = 1, b1 = 2, b2 = 1;
-    float y =  (b0 * x + b1 * x_pass[0] + b2 * x_pass[1] - a2 * y_pass[1])/a0; 
+    float y =  (b0 * x + b1 * x_pass[0] + b2 * x_pass[1] + a1 * y +  a2 * y_pass[1])/a0; 
     deslocaVetor(y_pass, 2, y);
     deslocaVetor(x_pass, 2, x);
     return y;
 }
-
-
 void setup() {
   Serial.begin(9600);
   pinMode(BOTAO, INPUT_PULLUP);
 }
-
 void loop() {
   if(digitalRead(BOTAO) == LOW) { 
     btnPress = !btnPress;
   }
-
   if(btnPress && time < TEMPO_MAX)
   {
     delay(50);
